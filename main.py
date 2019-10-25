60f4083f867af41ed72167a650ea5e19bb466e96
ArtemArkhipov1234 <aarkhipov2005@gmail.com>
2019-10-25 13:24:20 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 pass
 # test
 
+import turtle
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
