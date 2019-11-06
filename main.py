20c02789f3be19e700b74bcea6bb5c32fc769101
romanbrenner <55574495+romanbrenner@users.noreply.github.com>
2019-11-06 20:22:26 +0700

    Initial commit

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import turtle,time
 
 joe = turtle.Turtle()
 
-def starFILL (n, dlina)
+def starFILL (n, dlina) :
     joe.begin_fill()
     star(n,dlina)
     joe.end_fill()
 
-def star(n,dlina)
+def star(n,dlina) :
     if n % 2 != 0:
-        for i in range(n)
+        for i in range(n) :
             joe.forward(dlina)
             angle = n // 2 * 360 / n
             joe.left(angle)
 
-for i in range(5,16,2)
+for i in range(5,16,2) :
     joe.speed(10)
     starFILL(i,150)
     time.sleep(1)
