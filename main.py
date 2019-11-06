462478a3f43ec846bc2c5e903b6dbd8edd7e76df
romanbrenner <55574495+romanbrenner@users.noreply.github.com>
2019-11-06 19:24:18 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-pass
+import turtle
 
+joe = turtle.Turtle()
 
-# Рома-
+def starFILL(n, dlina)
+    joe.begin_fill()
+    star(n, dlina)
+    joe.end_fill()
+
+def star(n, dlina)
+    if n % 2 !=0
+        for i in range (n)
+            joe.forward(dlina)
+            angle = n // 2 * 360 / n
+            joe.left(angle)
+starFILL(9, 150)
+
