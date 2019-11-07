dc15744d8fd9499bfd715c1c16f7866b1f822541
ArtemArkhipov <aarkhipov2005@gmail.com>
2019-11-07 19:34:16 +0700

    Initial Commit

---
+++ Illusion.py
@@ -0,0 +1,26 @@
+import turtle
+
+from turtle import *
+
+bgcolor("gray")
+
+pu()
+speed(0)
+ht()
+shape("square")
+shapesize(3.2, 3.5)
+
+shift = [10, 0, 10, 28, 10, 0, 10, 28, 10]
+
+tracer(False)
+for i in range(9):
+    goto(-365 + shift[i], 267-66*i)
+    color("black")
+    for i in range(11):
+        stamp()
+        fd(70)
+        if pencolor() == "white":
+            color("black")
+        else:
+            color("white")
+mainloop()
