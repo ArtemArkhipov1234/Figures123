1aea5cfbc47fe02492319a254b8d06af058475dd
romanbrenner <55574495+romanbrenner@users.noreply.github.com>
2019-11-07 22:51:39 +0700

    Initial commit

--- Football.py
+++ Football.py
@@ -9,3 +9,14 @@ border.hideturtle()
 border.pensize(5)
 border.color('red')
 border.goto(300,300)
+border.down()
+border.goto(300,-300)
+border.goto(-300,-300)
+border.goto(-300,300)
+border.goto(300,300)
+
+ball = turtle.Turtle()
+ball.shape('circle')
+ball.up()
+dx = 3
+dy = 4
