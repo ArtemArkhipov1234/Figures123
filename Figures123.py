d912f060c7f20100eaec2e7f05bedd2e34db98d8
romanbrenner <55574495+romanbrenner@users.noreply.github.com>
2019-11-14 21:54:47 +0700

    Initial commit

--- Figures123.py
+++
@@ -1,9 +0,0 @@
-import turtle
-
-screen = turtle.Screen()
-
-screen.addshape(900,800)
-
-
-
-def Parallel():
--- Figures228.py
+++ Figures228.py
@@ -1,6 +1,7 @@
 import turtle
 
 screen = turtle.Screen()
+
 screen.addshape(900, 800)
 
 
@@ -64,7 +65,92 @@ def fish(x, y, size, color, angle):
 
 fish(-10, -200, "red",0 , 0)
 
-turtle.mainloop()
+
+def Parallel(x, y, size, angle):
+    turtle.reset()
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.down()
+
+    turtle.left(20)
+
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+
+    turtle.left(30)
+
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+
+    turtle.left(40)
+
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+
+    turtle.left(50)
+
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+
+    turtle.left(60)
+
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+
+    turtle.left(70)
+
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+
+    turtle.left(80)
+
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+    turtle.forward(size)
+    turtle.left(angle)
+
+
+Parallel(-300, 200, 50, 90)
 
 
 
