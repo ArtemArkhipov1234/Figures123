0a49afe7186dc7eca702ef9f77bffd482e68f53a
romanbrenner <55574495+romanbrenner@users.noreply.github.com>
2019-11-14 20:58:50 +0700

    Initial commit

--- Parallel.py
+++ Parallel.py
@@ -1,4 +1,14 @@
 import turtle
+
+screen = turtle.Screen()
+
+screen.addshape(900, 800)
+
+turtle.reset()
+turtle.up()
+turtle.goto(-300, 200)
+turtle.down()
+
 turtle.left(20)
 
 turtle.forward(50)
@@ -22,6 +32,50 @@ turtle.forward(50)
 turtle.left(90)
 
 turtle.left(40)
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+turtle.left(50)
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+turtle.left(60)
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+turtle.left(70)
+
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+
+turtle.left(80)
 
 turtle.forward(50)
 turtle.left(90)
