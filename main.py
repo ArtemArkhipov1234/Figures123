0ed3f31dc23132cc96207e4c9bfde61c9c749e9c
romanbrenner <55574495+romanbrenner@users.noreply.github.com>
2019-11-14 22:24:25 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import turtle,time
 
-joe = turtle.Turtle()
+screen = turtle.Screen()
 
+screen.addshape(900, 800)
+
+def star(x, y, n, dlina, color):
+    turtle.reset()
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.color(color)
+    turtle.forward(dlina)
+    angle = 80
+    turtle.left(angle)
+
+star(-300, -200, 9, 150, 'red')
 
